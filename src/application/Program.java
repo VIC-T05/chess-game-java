f2106dec48ecefec7fd7de2a9abc33117f47b9b8
unknown <victordzns1@gmail.com>
2024-12-03 16:43:19 -0300

    Board & Piece classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("test");
+		Board board = new Board(8, 8);
 		
+		System.out.println(board);
 	}
 }
